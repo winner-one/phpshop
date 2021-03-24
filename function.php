5dbdfb07d04106bd3601415d267bc17f08fc4a97
ONEWYL <1577714120@qq.com>
2021-03-24 14:07:01 +0800

    自定义函数文件

---
+++ function.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * @Author: Zhongyan Lin
+ * @Date: 2021-03-24 11:52:26
+ * @LastEditTime: 2021-03-24 14:02:39
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \phpshop\function.php
+ * 实现模拟对新文件的编辑行为
+ */
+
+    //显示标准格式化日期
+    function show_time(){
+        return date('Y-m-d H:i:s');
+    }
+    
+?>
